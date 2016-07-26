349b81fd2dbb3b0e79c1a2cb444160b3266e2dad
FrankLikuohao <frank.likuohao@gmail.com>
2016-07-26 12:10:39 +0800

    0726

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -2,12 +2,56 @@ package com.example.user.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    EditText editText;
+    RadioGroup radioGroup;
+
+    String sex = "male"
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.textView);
+        editText = (EditText)findViewById(R.id.editText);
+
+        radioGroup = (RadioGroup)findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId == R.id.maleradioButton)
+                {
+                    sex = "male";
+
+                }
+                else if(checkedId == R.id.femaleradioButton){
+                    sex = "female";
+                }
+            }
+        });
+        editText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.AC)
+                return false;
+            }
+        });
     }
+    public void submit(View view) {
+        String text = editText.getText().toString();
+        text = text + "©Ê§O"+sex;
+        textView.setText(text);
+
+        editText.setText("");
+
+
+    }
+
 }
